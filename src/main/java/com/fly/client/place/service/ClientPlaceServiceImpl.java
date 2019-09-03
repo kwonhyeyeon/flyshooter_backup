38beb393a37118a4f53c26a94c1feb9a290df293
InSub Lee <dldlstjq1212@gmail.com>
2019-09-03 11:03:54 +0900

    충돌해결

--- src/main/java/com/fly/client/place/service/ClientPlaceServiceImpl.java
+++ src/main/java/com/fly/client/place/service/ClientPlaceServiceImpl.java
@@ -3,27 +3,17 @@ package com.fly.client.place.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
 import org.springframework.beans.factory.annotation.Qualifier;
-=======
->>>>>>> 40299a269fce65f337823f3f163a5a74079df47d
 import org.springframework.stereotype.Service;
 
 import com.fly.client.place.dao.ClientPlaceDao;
 import com.fly.member.stadium.vo.StadiumVO;
 
-<<<<<<< HEAD
 @Service("clientPlaceService")
 public class ClientPlaceServiceImpl implements ClientPlaceService {
 	
 	@Autowired
 	@Qualifier("clientPlaceDao")
-=======
-@Service
-public class ClientPlaceServiceImpl implements ClientPlaceService {
-	
-	@Autowired
->>>>>>> 40299a269fce65f337823f3f163a5a74079df47d
 	private ClientPlaceDao clientPlaceDao;
 
 	// 구장별 경기장 리스트
@@ -31,8 +21,5 @@ public class ClientPlaceServiceImpl implements ClientPlaceService {
 	public List<StadiumVO> stadiumList(String p_name) {
 		return clientPlaceDao.stadiumList(p_name);
 	}
-<<<<<<< HEAD
-=======
 
->>>>>>> 40299a269fce65f337823f3f163a5a74079df47d
 }
--- src/main/java/com/fly/client/rental/controller/ClientRentalController.java
+++ src/main/java/com/fly/client/rental/controller/ClientRentalController.java
@@ -2,12 +2,8 @@ package com.fly.client.rental.controller;
 
 import java.util.List;
 
-<<<<<<< HEAD
 import javax.annotation.Resource;
 
-=======
-import org.springframework.beans.factory.annotation.Autowired;
->>>>>>> 40299a269fce65f337823f3f163a5a74079df47d
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -21,13 +17,8 @@ import com.fly.member.stadium.vo.StadiumVO;
 @Controller
 @RequestMapping(value="/rental")
 public class ClientRentalController {
-<<<<<<< HEAD
 
 	@Resource(name = "clientPlaceService")
-=======
-	
-	@Autowired
->>>>>>> 40299a269fce65f337823f3f163a5a74079df47d
 	private ClientPlaceService clientPlaceService;
 	
 	@RequestMapping(value="/rentalList.do", method = RequestMethod.GET)
