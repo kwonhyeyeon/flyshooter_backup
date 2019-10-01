a1b21e1901d2b296ef78d9117a1c111079183489
Simjeongmin <sja06159@naver.com>
2019-10-01 17:44:36 +0900

    구장,회원가입 스크립트 완료

--- src/main/webapp/resources/js/join.js
+++ src/main/webapp/resources/js/join.js
@@ -82,13 +82,17 @@ $(document).ready(function() {
 // 입력값 체크
 function joinCheck() {
    var re = /^[a-zA-Z0-9]{4,12}$/ // 아이디와 패스워드가 적합한지 검사할 정규식
-   var re2 = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-   // 이메일이 적합한지 검사할 정규식
+   var re2 = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;// 이메일이 적합한지 검사할 정규식
+   var re3 =  /^[A-Za-z0-9]{6,12}$/;//패스워드 정규식(숫자와 문자 포함 형태의 6~12자리 이내의 암호 정규식
+   var re4 = /^[가-힣]{2,50}$/;//이름 한글로 (2~50자)
+   var re5 = /^01([0|2|3|9]?)-?([0-9]{4})-?([0-9]{4})$/;// 핸드폰 번호 정규식
 
    var id = document.getElementById("m_id");
    var pw = document.getElementById("m_pw");
-
-
+   var name =  document.getElementById("m_name");
+   var phone = document.getElementById("m_phone");
+   //휴대폰번호 합치기
+   $("#m_phone").val($("#m_phone1").val()+'-'+$("#m_phone2").val()+'-'+$("#m_phone3").val());
    // ------------ 이메일 까지 -----------
 
    if (!check(re2, id, "적합하지 않은 ID 형식입니다.")) {
@@ -101,35 +105,34 @@ function joinCheck() {
    if (!check(re, pw, "패스워드는 4~12자의 영문 대소문자와 숫자로만 입력")) {
       return false;
    }
+   if(!check(re3, pw, "숫자와 문자 포함 형태의 6~12자리 이내")){
+	   return false;
+   }
+   
    if (pwConfirm == 1) {
       alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
       memberForm.m_pwCheck.focus();
       return false;
    }
-   if (memberForm.m_name.value == "") {
-      alert("이름을 입력해 주세요");
-      memberForm.m_name.focus();
-      return false;
-   }
-    
-   if (memberForm.m_phone1.value == "") {
-      alert("핸드폰번호 입력해 주세요1");
-      memberForm.m_phone1.focus();
-      return false;
+   if(!check(re4, name, "이름은 한글로 2자~50자 제한")){
+	   return false;
    }
    if (memberForm.m_phone2.value == "") {
-	      alert("핸드폰번호 입력해 주세요2");
+	      alert("핸드폰번호 입력해 주세요!");
 	      memberForm.m_phone2.focus();
 	      return false;
 	   }
    if (memberForm.m_phone3.value == "") {
-	      alert("핸드폰번호 입력해 주세요3");
+	      alert("핸드폰번호 입력해 주세요!!");
 	      memberForm.m_phone3.focus();
 	      return false;
 	   }
+   if(!check(re5, phone, "휴대폰 번호의 형식을 제대로 입력해주세요!")){
+	   return false;
+   }
+  
    
    idPwdCheck();
-   $("#m_phone").val($("#m_phone1").val()+'-'+$("#m_phone2").val()+'-'+$("#m_phone3").val());
    alert($("#m_phone").val());
    
 }
@@ -140,7 +143,8 @@ function check(re, what, message) {
     alert(message);
     what.value = "";
     what.focus();
-    //return false;
+   // return false;
+  
 }
 // 아이디에 비밀번호 포함 여부 확인
 function idPwdCheck() {
